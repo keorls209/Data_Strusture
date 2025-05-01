c9f99575f905ef2a2196445ccbeffb75d42767a1
KEROLOS AYMAN EBRAHIM ABDELMALEK <149635997+keorls209@users.noreply.github.com>
2025-05-01 12:49:41 +0300

    Update Edges.h
    Change setter fo verteses from ID to Name

--- Edges.h
+++ Edges.h
@@ -1,17 +1,19 @@
+#include<iostream>
+using namespace std;
 #pragma once
 class Edges
 {
 public:
-    int VertexID1;  
-    int VertexID2;  
+    string vertexName1;
+    string vertexName2;
     int Weight;     
 
-	Edges(int v1, int v2, int weight);
+    Edges(string n1,string n2 , int weight);
 	Edges();
-	void setEdgesValues(int v1, int v2, int weight);
+	void setEdgesValues(string n1, string n2, int weight);
     void setWeight(int weight);
-    int getVertexID1();
-    int getVertexID2();
+    string getVertexName1();
+    string getVertexName2();
     int getWeight();
 };
     
