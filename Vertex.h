946629095269b8491e9c4932d48efe56be82ae58
Makarious Magdy <114941517+Mokamagdy@users.noreply.github.com>
2025-05-11 12:00:33 +0300

    Remove id

--- Vertex.h
+++ Vertex.h
@@ -6,18 +6,13 @@ using namespace std;
 class Vertex
 {
 public:
-	int State_id;
 	string State_name;
 	list<Edges> edgeList;
-
 	Vertex();
-	Vertex(int id, string name);
+	Vertex(string name);
 	void addEdge(Edges& edge);
-	int getStateid();
 	string getStatename();
-	void setID(int id);
 	void setStateName(string name);
 	list<Edges>& getEdges();
 
 };
- 
